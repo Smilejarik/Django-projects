1796e16c501a4e34af96e8254f1ef34beb6fa099
Smilejarik <39190514+Smilejarik@users.noreply.github.com>
2020-03-19 22:06:00 +0100

    update

--- first_project/blog/urls.py
+++ first_project/blog/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 from . import views  # import views.py from the current dir
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),  # '' empty so this blog is home page, views.home is function
+    path('', PostListView.as_view(), name='blog-home'),  # '' empty so this blog is home page, views.home is function
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),  # '' empty so this blog is home page, views.home is function
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),  # '' empty so this blog is home page, views.home is function
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),  # '' empty so this blog is home page, views.home is function
+    path('post/new/', PostCreateView.as_view(), name='post-create'),  # '' empty so this blog is home page, views.home is function
     path('about/', views.about, name='blog-about'),  # 'about/' so about page, views.about function, /blog/about actually
 ]
